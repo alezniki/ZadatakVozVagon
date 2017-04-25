d6b540f80699b24ec58991eaab3594431dfc15f5
nikola <nikola@mail.com>
2017-04-25 18:23:29 +0200

    Kreirane klase za zadatke fixes #3

---
+++ src/main/java/zadaci/Zadatak1KreiranjeTabela.java
@@ -0,0 +1,31 @@
+package zadaci;
+
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * Created by androiddevelopment on 25.4.17..
+ */
+public class Zadatak1KreiranjeTabela {
+    public static void main(String[] args) {
+        ConnectionSource source = null;
+
+        try {
+            source = new JdbcConnectionSource("jdbc:sqlite:vozVagon.db");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (source != null) {
+                try {
+                    source.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
